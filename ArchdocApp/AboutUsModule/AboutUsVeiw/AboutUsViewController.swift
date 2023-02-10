11f07890e55ec36b2d886b4e9f7c765efbd8b7df
tixomark <tixomark@mail.ru>
2023-02-10 19:50:51 +0300

    added expanding cells

--- ArchdocApp/AboutUsModule/AboutUsVeiw/AboutUsViewController.swift
+++ ArchdocApp/AboutUsModule/AboutUsVeiw/AboutUsViewController.swift
@@ -8,71 +8,64 @@
 import UIKit
 
 class AboutUsViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var expandedCells: IndexSet = []
+    
+    let sectionData = ["This is text for Row 1", "This is text for Row 2", "This is text for Row 3"]
+   var selectedSection = -1
+    
+    var presenter: AboutUsPresenterProtocol!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-  @IBOutlet weak var tableView: UITableView!
+        tableView.register(AboutUsTableViewCell.self, forCellReuseIdentifier: AboutUsTableViewCell.cellID)
 
-  let sections = ["Section 1", "Section 2", "Section 3"]
-  let sectionData = [["This is text for Row 1", "This is text for Row 2"], ["This is text for Row 1"], ["This is text for Row 1", "This is text for Row 2", "This is text for Row 3"]]
-  var selectedSection = -1
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    tableView.dataSource = self
-    tableView.delegate = self
-  }
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
 }
 
+extension AboutUsViewController: AboutUsViewProtocol {
+
+}
+
+
 extension AboutUsViewController: UITableViewDataSource {
-  func numberOfSections(in tableView: UITableView) -> Int {
-    return sections.count
-  }
 
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    if section == selectedSection {
-      return sectionData[section].count
-    } else {
-      return 0
-    }
+      return sectionData.count
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-
-    let label = UILabel(frame: CGRect(x: 10, y: 0, width: tableView.bounds.size.width, height: tableView.bounds.size.height))
-    label.text = sectionData[indexPath.section][indexPath.row]
-    cell.addSubview(label)
-
+      let cell = tableView.dequeueReusableCell(withIdentifier: AboutUsTableViewCell.cellID, for: indexPath) as! AboutUsTableViewCell
+      
+      let cellData = sectionData[indexPath.row]
+      cell.setUpCell(usingData: cellData)
+      
     return cell
   }
 }
 
 extension AboutUsViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let headerView = UIView()
-        headerView.backgroundColor = UIColor.lightGray
-
-        let headerLabel = UILabel(frame: CGRect(x: 10, y: 5, width: tableView.bounds.size.width, height: tableView.bounds.size.height))
-        headerLabel.font = UIFont.boldSystemFont(ofSize: 16)
-        headerLabel.textColor = UIColor.black
-        headerLabel.text = sections[section]
-        headerView.addSubview(headerLabel)
-
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.headerTapped(_:)))
-        headerView.addGestureRecognizer(tapGesture)
-
-        return headerView
-    }
-
-    @objc func headerTapped(_ sender: UITapGestureRecognizer) {
-        let index = sender.view?.tag
-        if selectedSection == index {
-            selectedSection = index!
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! AboutUsTableViewCell
+        
+        if expandedCells.contains(indexPath.row) {
+            cell.collapse()
+            expandedCells.remove(indexPath.row)
+        } else {
+            cell.expand()
+            expandedCells.insert(indexPath.row)
         }
-        tableView.reloadData()
+
+        tableView.beginUpdates()
+        tableView.endUpdates()
+        
     }
 
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 30
-    }
 }
 
